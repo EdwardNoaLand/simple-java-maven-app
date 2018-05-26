0ffccea8e49f663e10d3998b3930476d117683a5
RuipengWang <rpwang@thoughtworks.com>
2018-05-26 18:05:07 +0800

    Add sample test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -40,6 +40,11 @@ public class AppTest
         }
     }
 
+    @Test
+    public void addSampleTest(){
+        Assert.assertEquals(true, true);
+    }
+
     @After
     public void cleanUpStreams() {
         System.setOut(null);
